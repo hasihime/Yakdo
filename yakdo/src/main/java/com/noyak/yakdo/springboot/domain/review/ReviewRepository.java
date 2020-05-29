922e0ab8d7088b6cc9d6122ca4af98d339c34773
wltmd0426 <wltmd0426@naver.com>
2020-05-29 17:45:05 +0900

    JS | 0529 | review read not work

--- yakdo/src/main/java/com/noyak/yakdo/springboot/domain/review/ReviewRepository.java
+++ yakdo/src/main/java/com/noyak/yakdo/springboot/domain/review/ReviewRepository.java
@@ -2,11 +2,20 @@ package com.noyak.yakdo.springboot.domain.review;
 
 import com.noyak.yakdo.springboot.domain.pharmacy.Pharmacy;
 import com.noyak.yakdo.springboot.domain.posts.Posts;
+import com.noyak.yakdo.springboot.web.dto.review.ReviewResponseDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     @Query(value = "select r.r_id from Review r where r.createdDate = (select max(r2.createdDate) from Review r2 where r2.p_id=:p_id)", nativeQuery = false)
     Long findReviewById(@Param("p_id") Pharmacy p_id);
+
+    @Query(value="select r.r_id, r.r_content, r.r_writer from Review r where r.p_id=:p_id order by r.createdDate desc", nativeQuery=false)
+    List<Review> findByPharmacy(@Param("p_id") Pharmacy p_id);
+
+//    @Query(value="update Review set content=(:review).r_content where r_id=:r_id", nativeQuery=false)
+//    void updateReview(@Param({"r_id", "r_content"]) Review review);
 }
--- yakdo/src/main/java/com/noyak/yakdo/springboot/web/ReviewController.java
+++ yakdo/src/main/java/com/noyak/yakdo/springboot/web/ReviewController.java
@@ -1,13 +1,20 @@
 package com.noyak.yakdo.springboot.web;
 
+import com.noyak.yakdo.springboot.domain.review.Review;
 import com.noyak.yakdo.springboot.result.LongResult;
 import com.noyak.yakdo.springboot.service.ReviewService;
+import com.noyak.yakdo.springboot.web.dto.review.ReviewModifyRequestDto;
+import com.noyak.yakdo.springboot.web.dto.review.ReviewResponseDto;
 import com.noyak.yakdo.springboot.web.dto.review.ReviewSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.xml.ws.Response;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,6 +23,20 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/review") // 앞에 공통으로 붙는 url
 public class ReviewController {
     private final ReviewService reviewService;
+
+    @GetMapping("/all/{p_id}")
+    public List<ReviewResponseDto> selectAllReview(@PathVariable int p_id) {
+        List<ReviewResponseDto> reviews = null;
+        System.out.println("약국 : "+p_id);
+        try {
+            // 각 약국마다 조회
+            reviews = reviewService.all(p_id);
+        } catch (RuntimeException e) {
+            log.error("selectAllReview", e);
+            throw e;
+        }
+        return reviews;
+    }
 
     @PostMapping("/create") // RequestMapping 뒤에 붙는  url
     public ResponseEntity<Object> createReview(@RequestBody ReviewSaveRequestDto requestDto) {
@@ -31,6 +52,44 @@ public class ReviewController {
             }
         } catch (RuntimeException e){
             log.error("createReview", e);
+            throw e;
+        }
+        return new ResponseEntity<Object>(lr, HttpStatus.OK);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Object> updateReview(@PathVariable Long id, @RequestBody ReviewModifyRequestDto requestDto) {
+        log.trace("review: {}", requestDto);
+        LongResult lr = null;
+        try {
+            boolean result = reviewService.modify(id, requestDto);
+            if(result) {
+//                Long r_id = reviewService.findReviewId(id);
+                lr = new LongResult("updateReview", id, "SUCCESS");
+            } else {
+                lr = new LongResult("updateReview", -1, "FAIL");
+            }
+        } catch (RuntimeException e) {
+            log.error("updateReview", e);
+            throw e;
+        }
+        return new ResponseEntity<Object> (lr, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<Object> deleteReview(@RequestBody ReviewSaveRequestDto requestDto) {
+        log.trace("review: {}", requestDto);
+        Long r_id = reviewService.findReviewId((requestDto.getP_id()));
+        LongResult lr = null;
+        try {
+            boolean result = reviewService.remove(requestDto);
+            if(result) {
+                lr = new LongResult("deleteReview", r_id, "SUCCESS");
+            } else {
+                lr = new LongResult("deleteReview", -1, "FAIL");
+            }
+        } catch (RuntimeException e) {
+            log.error("deleteReview", e);
             throw e;
         }
         return new ResponseEntity<Object>(lr, HttpStatus.OK);
