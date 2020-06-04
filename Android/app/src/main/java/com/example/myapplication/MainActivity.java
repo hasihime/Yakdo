f66b307c0477d8d729ad067e87d39cc501e9e975
91let <91let@naver.com>
2020-06-04 13:39:33 +0900

    added gps allow

--- Android/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Android/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.webkit.GeolocationPermissions;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebChromeClient;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,14 +27,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         webView = (WebView)findViewById(R.id.webView);
+        permissionCheck();
+    }
+
+    private void initWebView(){
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webSettings.setLoadWithOverviewMode(true);
         webSettings.setDomStorageEnabled(true);
-
-        // 앱에서 표시할 url 입력
+        // 자바스크립트 사용을 허용한다.
+        webView.setWebViewClient(new WebViewClient()); // 새로운 창을 띄우지 않고 내부에서 웹뷰를 실행시킨다.
+        webView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+                super.onGeolocationPermissionsShowPrompt(origin, callback);
+                callback.invoke(origin, true, false);
+            }
+        });
         webView.loadUrl("https://k02a4051.p.ssafy.io");
-        webView.setWebViewClient(new WebViewClient());
     }
 
     //폰의 뒤로가기 버튼의 동작 입력
@@ -44,4 +61,24 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MypharmacyActivity.class);
         startActivity(intent);
     }
+
+    private void permissionCheck(){
+        System.out.println("이거 실행은 됨.");
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            //Manifest.permission.ACCESS_FINE_LOCATION 접근 승낙 상태 일때
+            initWebView();
+        } else{ //Manifest.permission.ACCESS_FINE_LOCATION 접근 거절 상태 일때
+            // 사용자에게 접근권한 설정을 요구하는 다이얼로그를 띄운다.
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 0){
+            initWebView();
+        }
+    }
+
 }
