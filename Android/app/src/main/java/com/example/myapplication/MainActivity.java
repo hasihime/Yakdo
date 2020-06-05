ffd8aa9e5aeb89bd6d18712fd778f22f2a5c8c62
JH <belarus92@naver.com>
2020-06-05 14:59:00 +0900

    JH | 0605 | FIX WEBVIEW TEL INTENT ERROR

--- Android/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Android/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,8 +6,13 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -16,6 +21,9 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebChromeClient;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import java.net.URISyntaxException;
 
 public class MainActivity extends AppCompatActivity {
     public WebView webView;
@@ -36,7 +44,21 @@ public class MainActivity extends AppCompatActivity {
         webSettings.setLoadWithOverviewMode(true);
         webSettings.setDomStorageEnabled(true);
         // 자바스크립트 사용을 허용한다.
-        webView.setWebViewClient(new WebViewClient()); // 새로운 창을 띄우지 않고 내부에서 웹뷰를 실행시킨다.
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if( url.startsWith("http:") || url.startsWith("https:") ) {
+                    return false;
+                }
+                // tel일경우 아래와 같이 처리해준다.
+                else if (url.startsWith("tel:")) {
+                    Intent tel = new Intent(Intent.ACTION_DIAL, Uri.parse(url.replace('+',' ').trim()));
+                    startActivity(tel);
+                    return true;
+                }
+                return true;
+            }
+        }); // 새로운 창을 띄우지 않고 내부에서 웹뷰를 실행시킨다.
         webView.setWebChromeClient(new WebChromeClient(){
             @Override
             public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
@@ -63,22 +85,53 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void permissionCheck(){
-        System.out.println("이거 실행은 됨.");
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
             //Manifest.permission.ACCESS_FINE_LOCATION 접근 승낙 상태 일때
+            LocationManager locManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+            if(!locManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
+                alertCheckGPS();
+            }
             initWebView();
         } else{ //Manifest.permission.ACCESS_FINE_LOCATION 접근 거절 상태 일때
             // 사용자에게 접근권한 설정을 요구하는 다이얼로그를 띄운다.
             ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
         }
     }
-
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if(requestCode == 0){
+            LocationManager locManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+            if(!locManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
+                alertCheckGPS();
+            }
             initWebView();
         }
+    }
+    /** GPS 설정 유무 확인 **/
+    private void alertCheckGPS() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("GPS 기능이 꺼져있습니다 GPS기능을 켜시겠습니까?")
+                .setCancelable(false)
+                .setPositiveButton("GPS 설정하기",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                moveConfigGPS();
+                            }
+                        })
+                .setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+    /** GPS 설정화면으로 이동 **/
+    private void moveConfigGPS() {
+        Intent gpsOptionsIntent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+        gpsOptionsIntent.addCategory(Intent.CATEGORY_DEFAULT);
+        startActivity(gpsOptionsIntent);
     }
 
 }
